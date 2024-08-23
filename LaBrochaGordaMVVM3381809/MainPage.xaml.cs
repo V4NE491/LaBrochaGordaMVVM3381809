980c8d401f9803bc5a4e7b3207c829f652ec432a
MINEDUCYT <MINEDUCYT@DESKTOP-GLJR0B3>
2024-08-23 16:59:27 -0600

    Agregar archivos de proyecto.

---
+++ LaBrochaGordaMVVM3381809/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace LaBrochaGordaMVVM3381809
+{
+    public partial class MainPage : ContentPage
+    {
+       
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public class Cotizacion
+        {
+            public double PrecioPorMetroCuadrado { get; set; }
+            public double MetrosCuadrados { get; set; }
+            public double Total { get; set; }
+
+            public void CalcularTotal()
+            {
+                Total = PrecioPorMetroCuadrado * MetrosCuadrados;
+            }
+        }
+
+    }
+
+}
---
+++ LaBrochaGordaMVVM3381809/ViewModels/CotizacionViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using static LaBrochaGordaMVVM3381809.MainPage;
+
+namespace LaBrochaGordaMVVM3381809.ViewModels
+{
+    public class CotizacionViewModel : INotifyPropertyChanged
+    {
+        private Cotizacion _cotizacion;
+
+        public CotizacionViewModel()
+        {
+            _cotizacion = new Cotizacion();
+        }
+
+        public double PrecioPorMetroCuadrado
+        {
+            get => _cotizacion.PrecioPorMetroCuadrado;
+            set
+            {
+                _cotizacion.PrecioPorMetroCuadrado = value;
+                OnPropertyChanged();
+                CalcularTotal();
+            }
+        }
+
+        public double MetrosCuadrados
+        {
+            get => _cotizacion.MetrosCuadrados;
+            set
+            {
+                _cotizacion.MetrosCuadrados = value;
+                OnPropertyChanged();
+                CalcularTotal();
+            }
+        }
+
+        public double Total
+        {
+            get => _cotizacion.Total;
+            private set
+            {
+                _cotizacion.Total = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void CalcularTotal()
+        {
+            _cotizacion.CalcularTotal();
+            Total = _cotizacion.Total;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
